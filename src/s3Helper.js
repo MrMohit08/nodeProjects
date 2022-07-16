2088f767d11bf785f6bb023072f188eea6cb131c
MrMohit08 <44553552+MrMohit08@users.noreply.github.com>
2022-07-16 16:07:11 +0530

    aws s3 updated

--- src/s3Helper.js
+++ src/s3Helper.js
@@ -1,5 +1,5 @@
 const aws = require("aws-sdk") //load the SDK for javaScript-nodejs application
-const BookModel = require("../Models/bookModel")
+
 
 // aws credential data used by SDK, when the sdk for javascipt loads it automatically searches the credentials 
 aws.config.update({
@@ -21,22 +21,24 @@ let uploadFile = ( file) =>{
         Body: file.buffer //body specifies the buffer ,typed array, blob, string data type.
     }
    // uploading files to the bucket 
-    s3.upload(uploadParams, function (err, data){
+    s3.upload(uploadParams, function (err, data){  //callback function
         if(err) {
             console.log("Error", err)
         }
         if (data) {
-             console.log("upload Success", data.Location)
+             console.log(data)
+             console.log("file upload successfully", data.Location)
         }
     })
 }
 
-const bookCover = async function(req, res){
+let bookCover = async function(req, res){
     try{
         let files = req.files 
         if(files && files.length>0){
-          const uploadedFileUrl = await uploadFile(files[0])
-            res.status(201).send({msg: "file uploaded successfully", data: uploadedFileUrl})
+
+          let uploadedFileUrl = await uploadFile( files[0] )
+            res.status(201).send({message: "file uploaded successfully", data:uploadedFileUrl})
         }
         else{
             res.status(400).send({message: "no file found"})
