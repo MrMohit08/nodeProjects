887af860e39aa5ea873c4f7d3f2127ce5c8af5f9
MrMohit08 <44553552+MrMohit08@users.noreply.github.com>
2022-07-18 17:49:56 +0530

    few updates

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -6,7 +6,7 @@ const urlModel = require("../Model/urlModel")
 
 const isValid=function(value){
     if(typeof value === 'undefined' || value === null) return false
-    if(typeof value === 'string' && value.trim().length === 0) return false
+    if(typeof value !== 'string' || value.trim().length === 0) return false
     return true;
 } 
 
@@ -15,12 +15,12 @@ const isValid=function(value){
 const createUrl = async function(req, res) {
     try{
         const data = req.body;
-        const baseUrl = 'http:localhost:3000/'
+        const baseUrl = 'http://localhost:3000/'
 
     //Check if data is coming in request body
     if(Object.keys(data).length == 0){
        return res.status(400).send({
-        status: false, message: "Invalid URL Please Enter valid details"})
+        status: false, message: "Please Provide details"})
     }
     //check longUrl is present or not
     if(!data.longUrl){
@@ -33,10 +33,10 @@ const createUrl = async function(req, res) {
         status: false, message: 'It can not be null or undefined'})
      }   
 
-    // check if baseurl is not found
+    // check if baseurl is valid or not
     if(!validUrl.isUri(baseUrl)){
         return res.status(400).send({
-           status: false, message: "baseurl not found"}) 
+           status: false, message: "baseurl is not valid format"}) 
     }
     
   // check long url is valid or not 
@@ -48,50 +48,42 @@ const createUrl = async function(req, res) {
             status: false, message: "Already exists", data: url})
   }
    else {
-    let urlCode = shortid.generate().toLowerCase(); // generating the url code
-    //check if urlCode is present or not
+    let urlCode = shortid.generate().toLowerCase(); // generating the url code in lowercase 
+   //condition if urlCode is not generated
     if (!urlCode) {
-        console.log("urlCode is mandatory");
         return res.status(409).send({
-            status: false, message: "urlCode is required"})
+        status: false, message: "urlCode not generated"})
   }
-   //check if urlCode is valid format or not
-   if(!isValid(urlCode)){
-    return res.status(400).send({
-    status: false, message: 'urlCode is not valid format'})
-}  
+ 
 // check if urlCode is present in database
-let checkedUrlCode = await urlModel.findOne({urlCode:urlCode}).select({_id:0, __v:0,createdAt: 0, updatedAt: 0 });
+let checkedUrlCode = await urlModel.findOne({urlCode:urlCode})
 if (checkedUrlCode) {
-    return res.status(400).send({status: false, message: "ShortUrl is not unique"})}
+    return res.status(409).send({
+      status: false, message: "urlCode is exist in our database"})}
 
 let shortUrl = baseUrl + urlCode; // creating the short url 
    //check if short url is present or not
     if(!shortUrl){
         return res.status(400).send({
-          status: false, message: "shorturl is required"})
+          status: false, message: "shorturl is not created"})
     }
-    //check if short id is in valid format
-    if(!isValid(shortUrl)){
-        return res.status(400).send({
-        status: false, message: 'shortUrl is not valid format'})
-    }  
-     data.urlCode = urlCode
-     data.shortUrl = shortUrl
+    data.urlCode = urlCode
+    data.shortUrl = shortUrl
 
-     //check if shorturl is present in database
-     let getShortUrl = await urlModel.findOne({shortUrl:shortUrl})
-     if (getShortUrl) {
-        return res.status(400).send({status: false, message: "ShortUrl is not unique"})}
-
-     //creating a document in database
+    //creating a document in database
     let newUrl = await urlModel.create(data)
     if(!newUrl){
       return res.status(400).send({
         status : false,message :" No data found due to invalid request"})
   }
+
+  let finalUrl = { ...newUrl.toObject() }
+  delete finalUrl._id
+  delete finalUrl.__v
+  delete finalUrl.createdAt
+  delete finalUrl.updatedAt  
      res.status(201).send({
-     status: true, message: "Data created successfully", data : newUrl})
+     status: true, message: "Data created successfully", data : finalUrl})
     }
 }
 }
@@ -110,7 +102,7 @@ const getUrl = async function(req, res){
     const url = await urlModel.findOne({ urlCode: req.params.urlCode});
       if (url) {
           console.log("Redirecting to original url.....");
-          return res.redirect(url.longUrl);
+          return res.status(302).redirect(url.longUrl);
     } else {
           return res.status(404).send({ message: "No url found" });
         }
